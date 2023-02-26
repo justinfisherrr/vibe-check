d74101adad4aa65a1c2cc6d150bf806085c5eb0a
Alexis <alexismartin1895@gmail.com>
2023-02-26 01:04:06 -0800

    frontend

--- vibe-check-frontend/src/components/animation/Screen2.jsx
+++ vibe-check-frontend/src/components/animation/Screen2.jsx
@@ -5,7 +5,7 @@ function Screen2({animationData}){
         <h1 className='titles screen2-title'>MATCHING GENRES ARE</h1>
          <ul className='genre-list'>
             {animationData.screen2.genre.map((genre) => 
-                <li className='genre'>• {genre}</li>)
+                <li className='genre sub-texts'>• {genre}</li>)
             }
         </ul> 
         <h1>{JSON.stringify()}</h1>
--- vibe-check-frontend/src/components/animation/Screen4.jsx
+++ vibe-check-frontend/src/components/animation/Screen4.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
 
 function Screen4({animationData}){
-    return <h1>{JSON.stringify(animationData.screen4, null, 4)}</h1>
+    <div className='screen3-content'>
+                <div className='artist-covers'>
+                    {/* {animationData.screen3.artist[0].name} */}
+                    {animationData.screen3.artist.map((artistObj, index) => {
+                        return <div className={`artist-img-wrapper artist-img-wrapper-${index+1}`}>
+                                <img className="artist-img" src={artistObj.image} alt=""></img>
+                            </div>
+                    })}
+                </div>
+                <h1 className='titles screen3-title'>MATCHING ARTISTS ARE</h1>
+                <div className='artist-names'>
+                    {/* {animationData.screen3.artist[0].name} */}
+                    {animationData.screen3.artist.map((artistObj) => {
+                        return <p className="artist-name sub-texts">• {artistObj.name}</p>
+                    })}
+                </div>
+            </div>
 }
 
 export default Screen4
