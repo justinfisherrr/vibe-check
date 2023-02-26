5b430a4449a575c362c4140058efdfcfc4a51b20
justinfisherrr <censoredgreen@gmail.com>
2023-02-25 23:14:47 -0800

    Updates user if already exists

--- backend/helpers/addUser.js
+++ backend/helpers/addUser.js
@@ -1,17 +1,24 @@
 //A helper function that adds a user to our database
-const mongoose = require('mongoose');
-const User = require('../models/user');
-const spotifyParse = require('./spotifyParse');
+const mongoose = require("mongoose");
+const User = require("../models/user");
+const spotifyParse = require("./spotifyParse");
 
 async function addUser() {
   try {
     //Parse the data first
     const parsedData = await spotifyParse();
-    console.log(parsedData);
-    const user = new User(parsedData);
-    await user.save();
+    const foundProfile = await User.findOne({
+      username: parsedData.user_data.name,
+    });
+
+    if (foundProfile) {
+      await User.findOneAndUpdate(parsedData);
+    } else {
+      const user = new User(parsedData);
+      await user.save();
+    }
   } catch (error) {
-    console.log(error, 'Found in addUser');
+    console.log(error, "Found in addUser");
   }
 }
 
