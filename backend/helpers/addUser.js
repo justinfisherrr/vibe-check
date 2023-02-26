b6b5e68eda0a97f9cd38e698485572322271078f
justinfisherrr <censoredgreen@gmail.com>
2023-02-26 00:12:18 -0800

    Fixed username

--- backend/helpers/addUser.js
+++ backend/helpers/addUser.js
@@ -17,7 +17,7 @@ async function addUser() {
       const user = new User(parsedData);
       await user.save();
     }
-    return parsedData.user_data.username;
+    return parsedData.user_info.username;
   } catch (error) {
     console.log(error, "Found in addUser");
   }
