b084c5b26154a1e6913603b0209dd637bfa4608b
Alexis Martin <alexismartin1895@gmail.com>
2023-03-06 23:02:24 -0800

    results no longer go beyond bottom of page

--- vibe-check-frontend/src/components/search-results/SearchResults.jsx
+++ vibe-check-frontend/src/components/search-results/SearchResults.jsx
@@ -1,23 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import useWindowSize from '../window-size/useWindowSize';
 import './search-results.css';
 
 export default function SearchResults({ results }) {
-  return (
-    <div className='search-results-wrapper'>
-      {results &&
-        results.map(({ img, display_name, id }) => {
-          return (
-            <div className='result'>
-              <div className='result-img-wrapper'>
-                <img src={img} className='result-img' alt='' />
-              </div>
-              <div className='result-info-wrapper'>
-                <p className='result-displayname'>{display_name}</p>
-                <p className='result-id'>{id}</p>
-              </div>
-            </div>
-          );
-        })}
-    </div>
-  );
+	const windowSize = useWindowSize();
+	const [bottomOfScreen, setBottomOfScreen] = useState(0);
+
+	useEffect(() => {
+		const resultsWrapper = document.querySelector('.search-results-wrapper');
+		const resultsWrapperTop = resultsWrapper.getBoundingClientRect().top;
+
+		const bottomOfScreen = windowSize.height - resultsWrapperTop - 20;
+		setBottomOfScreen(bottomOfScreen);
+	}, [windowSize]);
+
+	return (
+		<div
+			className='search-results-wrapper'
+			style={{ maxHeight: bottomOfScreen }}>
+			{results &&
+				results.map(({ img, display_name, id }) => {
+					return (
+						<div className='result'>
+							<div className='result-img-wrapper'>
+								<img src={img} className='result-img' alt='' />
+							</div>
+							<div className='result-info-wrapper'>
+								<p className='result-displayname'>{display_name}</p>
+								<p className='result-id'>{id}</p>
+							</div>
+						</div>
+					);
+				})}
+		</div>
+	);
 }
