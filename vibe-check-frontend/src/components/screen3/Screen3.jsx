15c627fc5352361480daa7726e8444dde3e9a10e
Alexis Martin <alexismartin1895@gmail.com>
2023-03-12 01:06:09 -0800

    in the middle of top artist screen

--- vibe-check-frontend/src/components/screen3/Screen3.jsx
+++ vibe-check-frontend/src/components/screen3/Screen3.jsx
@@ -1,31 +1,69 @@
 import React from 'react';
 import './screen3.css';
+import sunBurst from '../../images/sun-burst.svg';
 
 function Screen3({ animationData }) {
 	return (
-		<div className='screen screen3-content'>
-			<div className='artist-covers'>
-				{animationData.screen3.artist.map((artistObj, index) => {
-					return (
-						<div
-							className={`overlap-img-wrapper overlap-img-wrapper-${
-								index + 1
-							}`}>
+		<div className='screen3-content'>
+			<h1 className='screen3-title fade-in'>Matching Artists Are</h1>
+			<div className='artist-grid'>
+				<div className='top-artist'>
+					<div className='top-artists-wrapper'>
+						<div className='top-artist-img-wrapper'>
 							<img
-								className='artist-img'
-								src={artistObj.artist_img}
-								alt=''></img>
+								className='top-artist-img'
+								src={animationData.screen3.artist[0].artist_img}
+								alt=''
+								srcset=''
+							/>
 						</div>
-					);
-				})}
-			</div>
-			<h1 className='titles screen3-title'>LIKE SONGS BY</h1>
-			<div className='artist-names'>
-				{animationData.screen3.artist.map((artistObj) => {
-					return (
-						<p className='artist-name sub-texts'>• {artistObj.artist_name}</p>
-					);
-				})}
+
+						<div className='sun-burst-wrapper'>
+							<img
+								src={sunBurst}
+								className='top-artist-burst'
+								alt=''
+								srcset=''
+							/>
+						</div>
+					</div>
+				</div>
+
+				<div className='remaining-artist'>
+					<div className='artist artist-2'>
+						<img
+							className='remaining-artist-img'
+							src={animationData.screen3.artist[1].artist_img}
+							alt=''
+							srcset=''
+						/>
+					</div>
+					<div className='artist artist-3'>
+						<img
+							className='remaining-artist-img'
+							src={animationData.screen3.artist[2].artist_img}
+							alt=''
+							srcset=''
+						/>
+					</div>
+					<div className='artist artist-4'>
+						<img
+							className='remaining-artist-img'
+							src={animationData.screen3.artist[3].artist_img}
+							alt=''
+							srcset=''
+						/>
+					</div>
+					<div className='artist artist-5'>
+						<img
+							className='remaining-artist-img'
+							src={animationData.screen3.artist[3].artist_img}
+							alt=''
+							srcset=''
+						/>
+					</div>
+				</div>
+				<div className='names'></div>
 			</div>
 		</div>
 	);
